c1067a969829b293bad9c0fa832916824b47f244
Chetan Barot <chetan@slickservices.in>
2021-12-06 16:41:06 +0530

    all bug solve only menu bar not show

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
+/**
+ * Template Name: Company - v4.7.0
+ * Template URL: https://bootstrapmade.com/company-free-html-bootstrap-template/
+ * Author: BootstrapMade.com
+ * License: https://bootstrapmade.com/license/
+ */
 (function() {
     "use strict";
 
@@ -14,37 +20,6 @@
     }
 
     /**
-     * Animation on scroll
-     */
-    window.addEventListener('load', () => {
-        AOS.init({
-            duration: 1000,
-            easing: 'ease-in-out',
-            once: true,
-            mirror: false
-        })
-    });
-
-    /**
-     * Mobile nav toggle
-     */
-    // on('click', '.mobile-nav-toggle', function(e) {
-    //     select('#navbar').classList.toggle('navbar-mobile')
-    //     this.classList.toggle('la-bars')
-    //     this.classList.toggle('bi-x')
-    // })
-
-    /**
-     * Mobile nav dropdowns activate
-     */
-    // on('click', '.navbar .dropdown > a', function(e) {
-    //     if (select('#navbar').classList.contains('navbar-mobile')) {
-    //         e.preventDefault()
-    //         this.nextElementSibling.classList.toggle('dropdown-active')
-    //     }
-    // }, true)
-
-    /**
      * Easy event listener function
      */
     const on = (type, el, listener, all = false) => {
@@ -59,41 +34,6 @@
     }
 
     /**
-     * Toggle .fixed-top class to #header when page is scrolled
-     */
-    let selectHeader = select('#header')
-    if (selectHeader) {
-        const headerScrolled = () => {
-                if (window.scrollY > 100) {
-                    selectHeader.classList.add('fixed-top')
-                } else {
-                    selectHeader.classList.remove('fixed-top')
-                }
-            }
-            // window.addEventListener('load', fixed - top)
-            // onscroll(document, fixed - top)
-    }
-
-    /**
-     * Scrolls to an element with header offset
-     */
-    const scrollto = (el) => {
-        let header = select('#header')
-        let offset = header.offsetHeight
-
-        if (!header.classList.contains('fixed-top')) {
-            offset -= 20
-        }
-
-        let elementPos = select(el).offsetTop
-        window.scrollTo({
-            top: elementPos - offset,
-            behavior: 'smooth'
-        })
-    }
-
-
-    /**
      * Easy on scroll event listener 
      */
     const onscroll = (el, listener) => {
@@ -103,20 +43,114 @@
     /**
      * Back to top button
      */
-    // let backtotop = select('.back-to-top')
-    // if (backtotop) {
-    //     const toggleBacktotop = () => {
-    //         if (window.scrollY > 100) {
-    //             backtotop.classList.add('active')
-    //         } else {
-    //             backtotop.classList.remove('active')
-    //         }
-    //     }
-    //     window.addEventListener('load', toggleBacktotop)
-    //     onscroll(document, toggleBacktotop)
-    // }
+    let backtotop = select('.back-to-top')
+    if (backtotop) {
+        const toggleBacktotop = () => {
+            if (window.scrollY > 100) {
+                backtotop.classList.add('active')
+            } else {
+                backtotop.classList.remove('active')
+            }
+        }
+        window.addEventListener('load', toggleBacktotop)
+        onscroll(document, toggleBacktotop)
+    }
 
+    /**
+     * Mobile nav toggle
+     */
+    on('click', '.mobile-nav-toggle', function(e) {
+        select('#navbar').classList.toggle('navbar-mobile')
+        this.classList.toggle('bi-list')
+        this.classList.toggle('bi-x')
+    })
 
+    /**
+     * Mobile nav dropdowns activate
+     */
+    on('click', '.navbar .dropdown > a', function(e) {
+        if (select('#navbar').classList.contains('navbar-mobile')) {
+            e.preventDefault()
+            this.nextElementSibling.classList.toggle('dropdown-active')
+        }
+    }, true)
+
+    /**
+     * Hero carousel indicators
+     */
+    let heroCarouselIndicators = select("#hero-carousel-indicators")
+    let heroCarouselItems = select('#heroCarousel .carousel-item', true)
+
+    heroCarouselItems.forEach((item, index) => {
+        (index === 0) ?
+        heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "' class='active'></li>":
+            heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "'></li>"
+    });
+
+    /**
+     * Porfolio isotope and filter
+     */
+    window.addEventListener('load', () => {
+        let portfolioContainer = select('.portfolio-container');
+        if (portfolioContainer) {
+            let portfolioIsotope = new Isotope(portfolioContainer, {
+                itemSelector: '.portfolio-item'
+            });
+
+            let portfolioFilters = select('#portfolio-flters li', true);
+
+            on('click', '#portfolio-flters li', function(e) {
+                e.preventDefault();
+                portfolioFilters.forEach(function(el) {
+                    el.classList.remove('filter-active');
+                });
+                this.classList.add('filter-active');
+
+                portfolioIsotope.arrange({
+                    filter: this.getAttribute('data-filter')
+                });
+                portfolioIsotope.on('arrangeComplete', function() {
+                    AOS.refresh()
+                });
+            }, true);
+        }
+
+    });
+    /**
+     * Animation on scroll
+     */
+    window.addEventListener('load', () => {
+        AOS.init({
+            duration: 1000,
+            easing: 'ease-in-out',
+            once: true,
+            mirror: false
+        })
+    });
+
+    /**
+     * Initiate portfolio lightbox 
+     */
+    const portfolioLightbox = GLightbox({
+        selector: '.portfolio-lightbox'
+    });
+
+    /**
+     * Portfolio details slider
+     */
+    new Swiper('.portfolio-details-slider', {
+        speed: 400,
+        loop: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
+        }
+    });
 
     /**
      * Skills animation
