b298700a1a1ab9e20ad09387631e9efa744420c1
Chetan Barot <chetan@slickservices.in>
2021-12-06 16:02:30 +0530

    js changs

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,6 +26,25 @@
     });
 
     /**
+     * Mobile nav toggle
+     */
+    on('click', '.mobile-nav-toggle', function(e) {
+        select('#navbar').classList.toggle('navbar-mobile')
+        this.classList.toggle('la-bars')
+        this.classList.toggle('bi-x')
+    })
+
+    /**
+     * Mobile nav dropdowns activate
+     */
+    on('click', '.navbar .dropdown > a', function(e) {
+        if (select('#navbar').classList.contains('navbar-mobile')) {
+            e.preventDefault()
+            this.nextElementSibling.classList.toggle('dropdown-active')
+        }
+    }, true)
+
+    /**
      * Easy event listener function
      */
     const on = (type, el, listener, all = false) => {
@@ -54,25 +73,6 @@
             // window.addEventListener('load', fixed - top)
             // onscroll(document, fixed - top)
     }
-
-    /**
-     * Mobile nav toggle
-     */
-    on('click', '.mobile-nav-toggle', function(e) {
-        select('#navbar').classList.toggle('navbar-mobile')
-        this.classList.toggle('la-bars')
-        this.classList.toggle('bi-x')
-    })
-
-    /**
-     * Mobile nav dropdowns activate
-     */
-    on('click', '.navbar .dropdown > a', function(e) {
-        if (select('#navbar').classList.contains('navbar-mobile')) {
-            e.preventDefault()
-            this.nextElementSibling.classList.toggle('dropdown-active')
-        }
-    }, true)
 
     /**
      * Scrolls to an element with header offset
