a732c6f6750c21f0e08c834c2d4b00cea4e68e97
Chetan Barot <chetan@slickservices.in>
2022-01-04 10:32:50 +0530

    changes

--- All Project HTMl/poit2cliam/assets/js/maindashboard.js
+++ All Project HTMl/poit2cliam/assets/js/maindashboard.js
@@ -115,24 +115,6 @@
 
     $(document).ready(function() {
         $("#myModal").modal('show');
-    });
-
-    var myModal = document.getElementById('myModal')
-    myModal.addEventListener('show.bs.modal', function(event) {
-        // Button that triggered the modal
-        var button = event.relatedTarget
-            // Extract info from data-bs-* attributes
-        var recipient = button.getAttribute('data-bs-whatever')
-            // If necessary, you could initiate an AJAX request here
-            // and then do the updating in a callback.
-            //
-            // Update the modal's content.
-        var modalTitle = myModal.querySelector('.modal-title')
-        var modalBodyInput = myModal.querySelector('.modal-body input')
-
-        modalTitle.textContent = 'New message to ' + recipient
-        modalBodyInput.value = recipient
-    })
-
+    });    
 
 })()
